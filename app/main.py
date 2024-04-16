628a9eb92827bd409edc7dab81f8c5835f2eb787
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-04-16 09:19:52 +0300

    refactoring

--- app/main.py
+++ app/main.py
@@ -34,18 +34,13 @@ async def main() -> None:
     await run_sequence(
         run_parallel(
             service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
-            run_sequence(
-                service.send_msg(
-                    Message(speaker_id, MessageType.PLAY_SONG,
-                            "Rick Astley - Never Gonna Give You Up")
-                ),
-                service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
-            ),
-            run_parallel(
-                service.send_msg(
-                    Message(hue_light_id, MessageType.SWITCH_OFF)),
-                service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF))
-            )
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
+        ),
+        service.send_msg(Message(speaker_id, MessageType.PLAY_SONG,
+                                 "Rick Astley - Never Gonna Give You Up")),
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF))
         ),
         run_sequence(
             service.send_msg(Message(toilet_id, MessageType.FLUSH)),
